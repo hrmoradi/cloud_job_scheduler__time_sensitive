951c6342a94a50b9870de13af421209a87d40932
hrmoradi <hrmoradi@gmail.com>
2017-12-21 01:31:44 -0600

    done with First Execution Option- needs to be refactored

--- Scheduler.py
+++ Scheduler.py
@@ -7,7 +7,11 @@ class ClassSchduler():
 
     def MainScheduler(jobList ):
         print("MainScheduler")
-        print(len(Set.jobList))
+        #print(len(Set.jobList))
+
+        """
+        defining required constants and variables
+        """
         queue = []
         failed=[]
 
@@ -27,12 +31,38 @@ class ClassSchduler():
         bid=3
         id = 4
 
+        collectedBid=0
+
+        pools = {}
+        for res in Set.resources:
+            pools[res[id]]=[]
+
+        """
+        Simulation Starts
+        """
         for du in range(Set.duration):                    # for specified time
             print("\nCurrent timeStamp: ",timeStamp," duration:",du)
 
 
+            for key in pools.keys():
+                thisPool=pools.get(key)
+                shouldBeRemoved = []
+                for job in thisPool:
+                    #reserved.append([waiting[execs][head][VMcore],waiting[execs][head][runtime],waiting[bid],waiting[id]])
+                    #print("b processtime",job)
+                    job[1]=job[1]-1
+                    #print("a processtime",job)
+                    if job[1]==0:
+                        print("                         job done with ID", job[3])
+                        for res in Set.resources:
+                            if res[id]==key:
+                                res[rCore]=res[rCore]+job[0]
+                        shouldBeRemoved.append(job)
+                for job in shouldBeRemoved:
+                    thisPool.remove(job)
 
             while (timeChecker<=timeStamp and len(Set.jobList)!=0):      # asses any job in list which received
+                print("     new jobs arrived", Set.jobList[head][id])
                 moveToQueu = Set.jobList[head] # [0,[[1,2,21],[2,2,11],[4,2,7]],26,5,1],   [s,[[e1],[e2],[e3]],d,b,id],
                 queue.append(moveToQueu)
                 del Set.jobList[head]
@@ -40,53 +70,91 @@ class ClassSchduler():
                     timeChecker = Set.jobList[head][arrival]
                 #print(Queu)
 
-
-
+            shouldBeRemoved = []
             for job in queue:                                 # remove dead jobs
                 for execution in job[execs]:
-                    if ( (timeStamp+execution[runtime]) > job[deadline]):
-                        print("          Execution Removed for job ID: ", job[id],"     ", (timeStamp+execution[runtime]) ,">", job[deadline],"     ",job)
+                    if ( (timeStamp+execution[runtime]-job[arrival]) > job[deadline]):
+                        print("     Execution Removed for job ID: ", job[id],"     ", (timeStamp+execution[runtime]) ,">", job[deadline],"     ",job)
                         job[execs].remove(execution)
+                        if Set.firstOptionOnly:
+                            shouldBeRemoved.append(job)
+                            print("           JOB Removed with ID: ", job[id], "     ", job)
                 if (len(job[execs])==0):
                     failed.append(job)
-                    print("     JOB Removed with ID: ", job[id],"     ",job)
-                    queue.remove(job)
+                    print("           JOB Removed with ID: ", job[id],"     ",job)
+                    shouldBeRemoved.append(job)
+            for job in shouldBeRemoved:
+                 queue.remove(job)
 
 
             #Sorted Queue
-            queue.sort(key=( lambda x: ( (timeStamp+x[execs][head][runtime])/(x[deadline]) ) ),reverse=True )
+            queue.sort(key=( lambda x: ( float(timeStamp+x[execs][head][runtime]-job[arrival])/float(x[deadline]) ) ),reverse=True )
             #print(queue)
 
             #address ability of job in waiting
-            for waiting in queue:
-                evaluateCurrentResource = copy.copy(Set.resources)
-                evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))), reverse=True)
+            waitingInPool=[]
+            shadowQueue =copy.deepcopy(queue)
+            for waiting in shadowQueue:
+                print("\n     waiting head: ",waiting)
+                ### <<<
+                evaluateCurrentResource = copy.deepcopy(Set.resources)
+                evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))))#, reverse=True)
                 VMs2address = waiting[execs][head][numVM]
                 for i in range(VMs2address):
-                    for res in Set.resources:
-                        if (res[rCore]>(waiting[execs][head][VMcore])):
+                    #print("VMs2address: ",VMs2address)
+                    for res in evaluateCurrentResource:
+                        #print("res: ",res)
+                        if (res[rCore]>=(waiting[execs][head][VMcore])):
                             VMs2address = VMs2address-1
-                            print("if b res[rCore]",res[rCore])
+                            #print("if b res[rCore]",res[rCore])
                             res[rCore]= res[rCore] - (waiting[execs][head][VMcore])
-                            print("if a res[rCore]", res[rCore])
+                            #print("if a res[rCore]", res[rCore])
                             break
-                    print("for res end res[rCore]", res[rCore])
-                    evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))),reverse=True)
+                    #print("for res end res[rCore]", res[rCore])
+                    evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))))#,reverse=True)
+                ### <<<
                 if VMs2address ==0:
-                    print("addressable job with ID: ", waiting[id])
+                    print("     current resources: ",Set.resources)
+                    print("     addressable job with ID: ", waiting[id])
                     # return(True)
                     # this job is addressable so apply
                     # repeat the process and put in pool
                     # update the resources
-                    
+                    ### >>>
+                    evaluateCurrentResource = copy.deepcopy(Set.resources)
+                    #print("evaluateCurrentResource Second:",evaluateCurrentResource)
+                    evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))))#, reverse=True)
+                    VMs2address = waiting[execs][head][numVM]
+                    for i in range(VMs2address):
+                        for res in evaluateCurrentResource:
+                            if (res[rCore] >= (waiting[execs][head][VMcore])):
+                                VMs2address = VMs2address - 1
+                                #print("if b res[rCore]", res[rCore])
+                                res[rCore] = res[rCore] - (waiting[execs][head][VMcore])
+                                #print("if a res[rCore]", res[rCore])
+                                reserved = pools[res[id]]
+                                reserved.append([waiting[execs][head][VMcore],waiting[execs][head][runtime],waiting[bid],waiting[id]])
+                                #print("res[id], reserved",res[id],res,reserved)
+                                pools[res[id]]=reserved
+                                break
+                        evaluateCurrentResource.sort(key=(lambda resource: (resource[rCore] - (waiting[execs][head][VMcore]))))#, reverse=True)
+                    waitingInPool.append([waiting])
+                    collectedBid=collectedBid+waiting[bid]
+                    print("     collected bid ",collectedBid)
+                    queue.remove(waiting)
+                    print("     !2 check Scalability:",waitingInPool)
+                    Set.resources = copy.deepcopy(evaluateCurrentResource)
+                    ### >>>
+                    print("     resources reduced: ", Set.resources)
                 else:
-                    print("not currently addressable job with ID: ", waiting[id])
+                    print("     Job not addressable with ID: ", waiting[id])
+                    print("     current resources: ", Set.resources)
 
 
 
             #print(Set.jobList)
             timeStamp=timeStamp+1
-            time.sleep(4)
+            time.sleep(1)
 
 
 
--- Settings.py
+++ Settings.py
@@ -14,14 +14,15 @@ maxScaleFactor=2
 #Scheduler Constants
 #resources = "~input/resources.txt"
 resources = [[4,8,0,0,1],[16,64,0,0,2]] #core,mem,0,0,id
-duration=42 # duration of running the app
+duration=44 # duration of running the app
+firstOptionOnly=True
 
 #sampleJob #            [s,[[e1],[e2],[e3]],d,b,id],
 #jobList = "~input/jobList.txt"
-jobList=[   [0,[[1,2,21],[2,2,11],[4,2,7]],26,5,1],
-            [0,[[2,2,27],[4,2,14]],29,10,2],
+jobList=[   [0,[[1,2,21],[2,2,11],[4,2,7]],23,5,1],#26
+            [0,[[2,2,27],[4,2,14]],29,10,2],#29
             [0,[[1,4,20],[2,4,11],[4,4,6]],24, 8,3],
-            [10, [[2,4,7], [2,4,4], [4,4,3]], 10, 4,4],
+            [10, [[1,4,7], [2,4,4], [4,4,3]], 10, 4,4],
             [10, [[1,8,9], [2,8,5]], 14, 8,5],
             [10, [[1,2,17], [2,2,10]], 27, 4,6],
             [20, [[1,4,15], [2,4,11]], 16, 6,7],
